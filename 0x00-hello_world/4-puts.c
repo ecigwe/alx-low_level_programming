d3498338ae1f3dbc532b3a73f4b20edb80feac85
ecigwe <emmanuel.igwe38@gmail.com>
2022-12-11 18:52:51 -0800

    msg: new msg

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,8 @@
  * main :entry
  * return :always 0
  */
-
 int main(void)
 {
-       	puts("\"Programming is like building a multilingual puzzle");  
+	puts("\"Programming is like building a multilingual puzzle");  
        	return (0);		 
 }
