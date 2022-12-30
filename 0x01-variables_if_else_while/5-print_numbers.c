7f6e3eb06a100b3e31b2df3ea9b575e80cd9a2ce
ecigwe <emmanuel.igwe38@gmail.com>
2022-12-30 17:52:24 +0000

    msg: number one to ten

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include  <stdio.h>
+
+/*
+ * main : returns digits
+ * returns : always returns 0
+ */
+
+int main(void)
+{
+	int number = 0;
+
+	for (number = 0; number < 10; number++)
+	{
+		printf("%d", number);
+	}
+	printf("\n");
+	return (0);
+}
