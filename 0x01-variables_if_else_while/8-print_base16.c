fa7b954a72b4aa52d440b90a4d3a3957d7b77f87
ecigwe <emmanuel.igwe38@gmail.com>
2022-12-31 13:58:54 +0000

    msg: file 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,7 +10,7 @@ int main(void)
 	int number = 0;
 	char alphabet = 'a';
 
-	for (number = 0; number <= 10; number++)
+	for (number = 0; number < 10; number++)
 		putchar((number % 10) + '0');
 
 	for (alphabet = 'a'; alphabet <= 'f'; alphabet++)
